c83f9eec88f3d32bc7ac52752b2b61f821c468c9
vergarapog <201801166@iacademy.edu.ph>
2021-09-12 11:12:23 +0800

    made agency first on portfolio section

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { useEffect, useState, useRef } from "react";
-import "./portofolio.scss";
-import PortofolioList from "../portofolioList/PortofolioList";
-import { sunnyside, restomenu, pomodoro, agency } from "../../data";
-import { gsap, Power3 } from "gsap";
+import React from "react"
+import { useEffect, useState, useRef } from "react"
+import "./portofolio.scss"
+import PortofolioList from "../portofolioList/PortofolioList"
+import { sunnyside, restomenu, pomodoro, agency } from "../../data"
+import { gsap, Power3 } from "gsap"
 
 const Portofolio = () => {
-  const [selected, setSelected] = useState("sunnyside");
-  const [data, setData] = useState(sunnyside);
+  const [selected, setSelected] = useState("agency")
+  const [data, setData] = useState(agency)
   const [activeLink, setActiveLink] = useState(
-    "https://sunnysideinc.netlify.app/"
-  );
+    "https://agency1234.netlify.app/"
+  )
   const [githubLink, setGithubLink] = useState(
-    "https://github.com/vergarapog/sunnyside-challenge"
-  );
-  const [btnData, setBtnData] = useState("Sunnyside");
+    "https://github.com/vergarapog/client-website-concept"
+  )
+  const [btnData, setBtnData] = useState("Sunnyside")
 
   const handleClick = (id) => {
-    setSelected(id);
-  };
+    setSelected(id)
+  }
 
-  let refBtns = useRef();
-  let refPics = useRef();
+  let refBtns = useRef()
+  let refPics = useRef()
 
   useEffect(() => {
     gsap.fromTo(
@@ -36,7 +36,7 @@ const Portofolio = () => {
         y: 0,
         ease: Power3.easeInOut,
       }
-    );
+    )
     gsap.fromTo(
       refBtns.current,
       {
@@ -49,19 +49,19 @@ const Portofolio = () => {
         y: 0,
         ease: Power3.easeInOut,
       }
-    );
-  }, [selected]);
+    )
+  }, [selected])
 
   const list = [
-    {
-      id: "sunnyside",
-      title: "Sunnyside Landing Page",
-      link: "youtube.com",
-    },
     {
       id: "agency",
       title: "Theoretical Client Site",
       link: "",
+    },
+    {
+      id: "sunnyside",
+      title: "Sunnyside Landing Page",
+      link: "youtube.com",
     },
     {
       id: "restomenu",
@@ -73,43 +73,43 @@ const Portofolio = () => {
       title: "Pomodoro Web App",
       link: "",
     },
-  ];
+  ]
 
   useEffect(() => {
     switch (selected) {
       case "sunnyside":
-        setData(sunnyside);
-        setActiveLink("https://sunnysideinc.netlify.app/");
-        setGithubLink("https://github.com/vergarapog/sunnyside-challenge");
-        setBtnData("Sunnyside");
+        setData(sunnyside)
+        setActiveLink("https://sunnysideinc.netlify.app/")
+        setGithubLink("https://github.com/vergarapog/sunnyside-challenge")
+        setBtnData("Sunnyside")
 
-        break;
+        break
       case "agency":
-        setData(agency);
-        setActiveLink("https://agency1234.netlify.app/");
-        setGithubLink("https://github.com/vergarapog/client-website-concept");
-        setBtnData("Agency");
+        setData(agency)
+        setActiveLink("https://agency1234.netlify.app/")
+        setGithubLink("https://github.com/vergarapog/client-website-concept")
+        setBtnData("Agency")
 
-        break;
+        break
       case "restomenu":
-        setData(restomenu);
-        setActiveLink("https://jabeefavs.netlify.app/");
-        setGithubLink("https://github.com/vergarapog/RestaurantMenu-ReactJS");
-        setBtnData("Jolibee fav");
+        setData(restomenu)
+        setActiveLink("https://jabeefavs.netlify.app/")
+        setGithubLink("https://github.com/vergarapog/RestaurantMenu-ReactJS")
+        setBtnData("Jolibee fav")
 
-        break;
+        break
       case "pomodoro":
-        setData(pomodoro);
-        setActiveLink("https://pomodoro-todo.netlify.app/");
-        setGithubLink("https://github.com/vergarapog/pomo-todo");
-        setBtnData("Pomodoro");
+        setData(pomodoro)
+        setActiveLink("https://pomodoro-todo.netlify.app/")
+        setGithubLink("https://github.com/vergarapog/pomo-todo")
+        setBtnData("Pomodoro")
 
-        break;
+        break
 
       default:
-        setData(sunnyside);
+        setData(sunnyside)
     }
-  }, [selected]);
+  }, [selected])
 
   return (
     <div className="portofolio" id="portofolio">
@@ -124,7 +124,7 @@ const Portofolio = () => {
                 isActive="active"
                 handleClick={handleClick}
               />
-            );
+            )
           }
           return (
             <PortofolioList
@@ -132,7 +132,7 @@ const Portofolio = () => {
               title={item.title}
               handleClick={handleClick}
             />
-          );
+          )
         })}
       </ul>
       __________________________________________
@@ -153,11 +153,11 @@ const Portofolio = () => {
                 <h3>{item.title}</h3>
               </div>
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Portofolio;
+export default Portofolio
