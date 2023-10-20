fcd90bf41feaec17fc6ada49540fd74b864148ce
vergarapog <201801166@iacademy.edu.ph>
2023-10-20 22:04:08 +0800

    updated project descriptions

--- src/components/portofolio/ProjectsList.js
+++ src/components/portofolio/ProjectsList.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "CSS Animations Generator",
     description:
-      "My main goal on this internship was to create a web application wherein the user can experiment with a bunch of animations and then generate their corresponding CSS code, which the designers and developers at my Internship company could use and reference in development.",
+      "I've faithfully recreated animista.net, a widely-used CSS animation tool, as a demonstration of my frontend development skills and attention to detail. This project showcases my ability to replicate popular web applications, emphasizing my proficiency in creating user-friendly interfaces and interactive web tools. I've added features like tailwind code generation support and a draggable object to enhance the user experience. Explore the project via the link below to witness the capabilities I can bring to your team.",
     links: {
       website: "https://vue-css-animations.netlify.app/",
       github: "",
@@ -10,9 +10,9 @@ export const projects = [
   },
 
   {
-    title: "Second Proj",
+    title: "REST Countries API with Dark Mode",
     description:
-      "My main goal on this internship was to create a web application...",
+      "I created a modern and responsive web application that integrates with the REST Countries API, allowing users to explore country data effortlessly. Users can search for countries, filter by region, and access detailed information about each country, including bordering countries. Additionally, I added a dark mode option to enhance the user experience. Check out my project to experience this feature-rich country explorer.",
     links: {
       website: "https://vue-css-animations.netlify.app/",
       github: "",
@@ -22,10 +22,12 @@ export const projects = [
   {
     title: "Third Proj",
     description:
-      "My main goal on this internship was to create a web application...",
+      "I've successfully recreated the classic Rock, Paper, Scissors game. Users can engage in a thrilling match against the computer while enjoying a responsive interface that adapts to their device's screen size. Additionally, I've implemented interactive hover and focus states for a seamless user experience. Challenge me to a game, and let's see who comes out victorious!",
     links: {
       website: "https://vue-css-animations.netlify.app/",
       github: "",
     },
   },
 ];
+
+// My main goal on this internship was to create a web application wherein the user can experiment with a bunch of animations and then generate their corresponding CSS code, which the designers and developers at my Internship company could use and reference in development.
