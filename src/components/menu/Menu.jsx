6bad4cec2f5ad24702e1a0a0cde5f857009f1380
vergarapog <201801166@iacademy.edu.ph>
2024-04-10 16:32:36 +0800

    chore: enhance sidebar 2

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -10,40 +10,50 @@ const Menu = ({ menuValue, setMenu }) => {
         "menu " + (menuValue && "active") + " " + (isDarkMode && "dark-mode")
       }
     >
-      <ul className={`${isDarkMode ? "dark-mode" : ""}`}>
-        <li className="menu-item" onClick={() => setMenu(!menuValue)}>
-          <a href="#intro">Home</a>
-        </li>
-        <li className="menu-item" onClick={() => setMenu(!menuValue)}>
-          <a href="#portofolio">Portfolio</a>
-        </li>
-        <li className="menu-item" onClick={() => setMenu(!menuValue)}>
-          <a href="#skills">About Me</a>
-        </li>
-        <li className="menu-item" onClick={() => setMenu(!menuValue)}>
-          <a href="#contact">Contact</a>
-        </li>
-        <li className="menu-item" onClick={() => setMenu(!menuValue)}>
-          <a
-            href="https://drive.google.com/file/d/1kK8_ZKfcuBNWYDwF8tTw0Cn5Fe6Kdw8p/view?usp=drive_link"
-            target="_blank"
-            rel="noreferrer"
-            className="docu-links"
-          >
-            My Resume
-          </a>
-        </li>
-        <li className="menu-item" onClick={() => setMenu(!menuValue)}>
-          <a
-            href="https://github.com/vergarapog"
-            target="_blank"
-            rel="noreferrer"
-            className="docu-links"
-          >
-            My Github
-          </a>
-        </li>
-      </ul>
+      <div className="sidebar-header">
+        <div className="close-btn" onClick={() => setMenu(!menuValue)}>
+          X
+        </div>
+      </div>
+      <div className="sidebar-container">
+        <div className="sidebar-logo" onClick={() => setMenu(!menuValue)}>
+          <a href="#intro">Brian.</a>
+        </div>
+        <ul className={`${isDarkMode ? "dark-mode" : ""}`}>
+          {/* <li className="menu-item" onClick={() => setMenu(!menuValue)}>
+            <a href="#intro">Home</a>
+          </li> */}
+          <li className="menu-item" onClick={() => setMenu(!menuValue)}>
+            <a href="#portofolio">Portfolio</a>
+          </li>
+          <li className="menu-item" onClick={() => setMenu(!menuValue)}>
+            <a href="#skills">About Me</a>
+          </li>
+          <li className="menu-item" onClick={() => setMenu(!menuValue)}>
+            <a href="#contact">Contact</a>
+          </li>
+          <li className="menu-item" onClick={() => setMenu(!menuValue)}>
+            <a
+              href="https://drive.google.com/file/d/1kK8_ZKfcuBNWYDwF8tTw0Cn5Fe6Kdw8p/view?usp=drive_link"
+              target="_blank"
+              rel="noreferrer"
+              className="docu-links"
+            >
+              My Resume
+            </a>
+          </li>
+          <li className="menu-item" onClick={() => setMenu(!menuValue)}>
+            <a
+              href="https://github.com/vergarapog"
+              target="_blank"
+              rel="noreferrer"
+              className="docu-links"
+            >
+              My Github
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
