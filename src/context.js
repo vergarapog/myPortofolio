0d2a7de627eb862963c0acdaca8b56d056c32394
Brian Vergara <vbrian.dev@gmail.com>
2025-05-08 21:40:08 +0800

    fix color shift on first render

--- src/context.js
+++ src/context.js
@@ -4,7 +4,16 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [isNavDark, setIsNavDark] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    // Check if window is defined (to avoid SSR issues)
+    if (typeof window !== "undefined") {
+      return (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      );
+    }
+    return false;
+  });
 
   return (
     <AppContext.Provider
